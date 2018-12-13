c0f4a174fcc69db70019026770215d608c9beffa
Welliton Klein <welliton.fokushima@gmail.com>
2018-12-13 20:06:13 -0200

    Adicionando Bootstrap [CSS]

--- src/main.js
+++ src/main.js
@@ -3,11 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 
-Vue.config.productionTip = false
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
+  render: h => h(App)
 })
