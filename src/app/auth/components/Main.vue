d3755544c186983d22c493fe64abe43094c4809e
Welliton Klein <welliton.fokushima@gmail.com>
2018-12-13 23:35:49 -0200

    Login form

--- src/app/auth/components/Main.vue
+++ src/app/auth/components/Main.vue
@@ -1,10 +1,16 @@
 <template>
-  <header class="page-header row">
-    <h3>Confee [Auth]</h3>
-  </header>
+  <div>
+    <header class="page-header row">
+      <h3>Confee</h3>
+    </header>
+    <Login />
+  </div>
 </template>
 
 <script>
+  import Login from './Forms/Login'
   export default {
+    name: 'Auth',
+    components: { Login }
   }
 </script>
