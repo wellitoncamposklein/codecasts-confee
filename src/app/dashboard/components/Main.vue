47fb71619c53a7ca38d9885862213af4d0f78e45
Welliton Klein <welliton.fokushima@gmail.com>
2018-12-13 23:08:16 -0200

    camada http

--- src/app/dashboard/components/Main.vue
+++ src/app/dashboard/components/Main.vue
@@ -1,10 +1,20 @@
 <template>
   <header class="page-header row">
     <h3>Confee</h3>
+    <!--<pre>{{  }}</pre>-->
   </header>
 </template>
 
 <script>
+  import http from 'src/http'
+
   export default {
+    mounted () {
+      http.get('https://pokeapi.co/api/v2/pokemon/1/')
+        .then(response => response.data)
+        .then(data => {
+          console.log(data)
+        })
+    }
   }
 </script>
