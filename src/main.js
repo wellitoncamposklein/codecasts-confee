a8b51c43542ac963fab460f3cfb591935e84fac4
Welliton Klein <welliton.fokushima@gmail.com>
2018-12-13 22:50:43 -0200

    Refatorando

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router/index'
-import store from './vuex/index'
+import router from './router'
+import store from './vuex'
 
 /* eslint-disable no-new */
 new Vue({
