09485a04556d4460011daa3df657d2e40b786b39
Welliton Klein <welliton.fokushima@gmail.com>
2018-12-13 22:07:48 -0200

    Bootstrap collapse navbar

---
+++ src/components/root/header/main.vue
@@ -0,0 +1,30 @@
+<script>
+  import Left from './left.vue'
+  import Brand from './brand.vue'
+
+  export default {
+    name: 'confee-header',
+    components: { Left, Brand },
+    data () {
+      return { isOpen: false }
+    },
+    methods: {
+      onToggleMenu () { this.isOpen = !this.isOpen }
+    }
+  }
+</script>
+
+<template>
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <Brand @toggle="onToggleMenu" />
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" :class="{ in: isOpen }">
+        <Left />
+      </div>
+      <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+  </nav>
+</template>
